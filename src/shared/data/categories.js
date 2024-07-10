74131c9339fdfafa5e3b995e48da783ba8f44489
P1ST-0N <luckylionia@gmail.com>
2024-07-10 20:54:17 +0300

    add folder data

---
+++ src/shared/data/categories.js
@@ -0,0 +1,24 @@
+const dbCategories = [
+  {
+    key: 'all',
+    name: 'All',
+  },
+  {
+    key: 'dark',
+    name: 'Dark chocolate',
+  },
+  {
+    key: 'gift',
+    name: 'Gift box',
+  },
+  {
+    key: 'milk',
+    name: 'Milk chocolate',
+  },
+  {
+    key: 'white',
+    name: 'White chocolate',
+  },
+];
+
+export default dbCategories;
