f1b0534e8df2ad836503732aea757b4cea94d7e0
P1ST-0N <luckylionia@gmail.com>
2024-07-12 12:34:25 +0300

    add modules/cart/components/cart

---
+++ src/modules/cart/components/Cart/Cart.jsx
@@ -0,0 +1,18 @@
+import EmptyCart from '../EmptyCart/EmptyCart';
+import { useSelector } from 'react-redux';
+import YourCart from '../YourCart/YourCart';
+
+const Cart = ({ closeCartModal, isOpen }) => {
+  const products = useSelector((state) => state.cart.products);
+  return (
+    <>
+      {products.length > 0 ? (
+        <YourCart closeCartModal={closeCartModal} isOpen={isOpen} />
+      ) : (
+        <EmptyCart closeCartModal={closeCartModal} />
+      )}
+    </>
+  );
+};
+
+export default Cart;
