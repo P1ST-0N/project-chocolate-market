4f88f81e2be70501f7d71149a00474637ba8f748
P1ST-0N <luckylionia@gmail.com>
2024-07-12 19:31:03 +0300

    add helpers funcs

---
+++ src/helpers/scrollToElementById.js
@@ -0,0 +1,13 @@
+export const scrollToElementById = (id) => {
+  const getSectionById = document.querySelector(`#${id}`);
+
+  if (!getSectionById) return;
+
+  const topOffset = 20;
+  const topPosition = getSectionById.offsetTop - topOffset;
+
+  window.scrollTo({
+    top: topPosition,
+    behavior: 'smooth',
+  });
+};
