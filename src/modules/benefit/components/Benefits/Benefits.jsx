e6319b923f2269d9ca7f221ba1610913b7e673b6
P1ST-0N <luckylionia@gmail.com>
2024-07-12 12:21:11 +0300

    updt modules/benefit

--- src/modules/benefit/components/Benefits/Benefits.jsx
+++ src/modules/benefit/components/Benefits/Benefits.jsx
@@ -1,26 +1,58 @@
-// import benefits from 'modules/benefit/helpers/benefits';
-// import { Container } from 'shared/components';
-// import style from './Benefits.module.scss';
-// import { icons as sprite } from 'shared/icons';
-// import { imagesDesktop2x } from 'shared/images';
-// import SectionMain from 'shared/components/SectionMain/SectionMain';
-// import { useMedia } from 'hooks/index';
+import benefits from 'modules/benefit/helpers/benefits';
+import { Container } from 'shared/components';
+import style from './Benefits.module.scss';
+import { icons as sprite } from 'shared/icons';
+import { imagesDesktop2x } from 'shared/images';
+import SectionMain from 'shared/components/SectionMain/SectionMain';
+import { useMedia } from 'hooks/index';
 
-// const Benefits = () => {
-//   const { isTablet, isDesktop } = useMedia();
+const Benefits = () => {
+  const { isTablet, isDesktop } = useMedia();
 
-//   return (
-//     <SectionMain className={style.benefitsSection}>
-//       <h2 className={style.visuallyHidden}>Benefits</h2>
-//       <div className={style.benefitsThumb}>
-//         {isTablet && isDesktop && (
-//           <img
-//             className={style.benefitsImgUp}
-//             src={imagesDesktop2x.lineUp2x}
-//             alt="line"
-//           />
-//         )}
-//       </div>
-//     </SectionMain>
-//   );
-// };
+  return (
+    <SectionMain className={style.benefitsSection}>
+      <h2 className={style.visuallyHidden}>Benefits</h2>
+      <div className={style.benefitsThumb}>
+        {isTablet && isDesktop && (
+          <img
+            className={style.benefitsImgUp}
+            src={imagesDesktop2x.lineUp2x}
+            alt="line"
+          />
+        )}
+        <Container>
+          <ul className={style.benefitsList}>
+            {benefits.map((benefit, index) => (
+              <li
+                data-aos="zoom-in-down"
+                data-aos-easing="linear"
+                data-aos-duration="700"
+                key={index}
+                className={style.benefitsItem}
+              >
+                <div className={style.thumbBenefits}>
+                  <svg className={style.benefitsIcons}>
+                    <use xlinkHref={`${sprite}#${benefit.icon}`} />
+                  </svg>
+                </div>
+                <h3 className={style.benefitsTitle}>{benefit.title}</h3>
+                <p className={style.benefitsDescription}>
+                  {benefit.describtion}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </Container>
+        {(isTablet || isDesktop) && (
+          <img
+            className={style.benefitsImgDown}
+            src={imagesDesktop2x.lineDown2x}
+            alt="line"
+          />
+        )}
+      </div>
+    </SectionMain>
+  );
+};
+
+export default Benefits;
