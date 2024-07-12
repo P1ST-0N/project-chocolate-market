f82b5f71f66c268ed432cc7b856f0916b47c73de
P1ST-0N <luckylionia@gmail.com>
2024-07-12 20:21:04 +0300

    create formSlice & store

---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { persistedCartReducer } from './cart/cartSlice';
+import { persistedFormReducer } from './form/formSlice';
+
+export const store = configureStore({
+  reducer: {
+    cart: persistedCartReducer,
+    form: persistedFormReducer,
+  },
+  middleware: (gdm) =>
+    gdm({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistore = persistStore(store);
