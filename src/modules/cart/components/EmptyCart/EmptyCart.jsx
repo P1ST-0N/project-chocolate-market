03d9d1a9f77a98bd8e47a02c11f3bcb9f3678841
P1ST-0N <luckylionia@gmail.com>
2024-07-12 12:55:03 +0300

    added emptycart

---
+++ src/modules/cart/components/EmptyCart/EmptyCart.jsx
@@ -0,0 +1,32 @@
+import style from './EmptyCart.module.scss';
+
+import { scrollToElementById } from 'helpers/scrollToElementById';
+import { Button } from 'shared/components';
+
+const EmptyCart = ({ closeCartModal }) => {
+  const handleBuyNowClick = () => {
+    scrollToElementById('ourProducts');
+    closeCartModal();
+  };
+
+  return (
+    <div className={style.emptyCart}>
+      <h2 className={style.mainTitleCart}>Your shopping cart</h2>
+      <div className={style.imgCart}></div>
+      <div className={style.wrapperInform}>
+        <h3 className={style.cartTitle}>The basket is empty</h3>
+        <p className={style.cartDescription}>
+          See our catalog and choose delicious chocolate
+        </p>
+        <span className={style.cartLine}></span>
+        <Button
+          title={'Buy now'}
+          className={style.btnOrangeStyle}
+          onClick={handleBuyNowClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EmptyCart;
