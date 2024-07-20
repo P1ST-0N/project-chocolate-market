d2604b9fbe6c95803c39c57a54158401897a61c5
P1ST-0N <luckylionia@gmail.com>
2024-07-21 00:47:18 +0300

    create SharedLayout component

---
+++ src/shared/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,22 @@
+import { Footer } from 'modules/footer';
+import { Header } from 'modules/header';
+import { ArrowUp } from '..';
+import { useMediaQuery } from 'react-responsive';
+import style from './SharedLayout.module.scss';
+import { Toaster } from 'react-hot-toast';
+
+const SharedLayout = ({ children }) => {
+  const isDesktop = useMediaQuery({ query: '(min-width: 1440px)' });
+
+  return (
+    <div className={style.oveflow}>
+      <Header />
+      {children}
+      <Footer />
+      {isDesktop && <ArrowUp />}
+      <Toaster position="top-right" reverseOrder={true} />
+    </div>
+  );
+};
+
+export default SharedLayout;
