4aa8208bc922cf5268c1a1c6f7300838f053828d
P1ST-0N <luckylionia@gmail.com>
2024-07-21 00:51:19 +0300

    create SocialMedia

---
+++ src/shared/components/SocialMedia/SocialMedia.jsx
@@ -0,0 +1,29 @@
+import { icons as sprite } from 'shared/icons';
+import style from './SocialMedia.module.scss';
+
+const SocialMedia = ({ className }) => {
+  return (
+    <div className={className}>
+      <div>
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          aria-label="Instagram"
+        >
+          <svg className={style.socialIcon}>
+            <use xlinkHref={`${sprite}#instagram`} />
+          </svg>
+        </a>
+      </div>
+      <div>
+        <a href="https://x.com/" target="_blank" aria-label="Twitter">
+          <svg className={style.socialIcon}>
+            <use xlinkHref={`${sprite}#twitter`} />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
